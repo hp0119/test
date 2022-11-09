cdbe1d9f6a3d0c36e52023691e34693701d7e132
hp0119 <2205883@uad.ac.uk>
2022-11-09 16:08:01 +0000

    update

---
+++ CMP307 Software Engineering - Scottish Glen/CMP307 Software Engineering - Scottish Glen/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CMP307_Software_Engineering___Scottish_Glen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
